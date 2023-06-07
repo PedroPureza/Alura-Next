94bf0ffdfe9581cc1908172905f990ab464f35eb
Pedro Pureza <pedro.pureza@poatek.com>
2023-06-07 15:10:40 -0300

    add styled link component

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,10 +1,11 @@
-import NextLink from 'next/link';
+// Componente Link
+import NextLink from "next/link";
+import StyledLink from "../StyledLink";
 
-export default function Link({children,href, ...props}) {
-    return(
-        <NextLink href={href} passHref>
-            <a {...props}>{children}</a>
-        </NextLink>
-    )
-
+export default function Link({ children, href, ...props }) {
+  return (
+    <NextLink href={href}>
+      <StyledLink {...props}>{children}</StyledLink>
+    </NextLink>
+  );
 }
