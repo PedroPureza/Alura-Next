7c0eb41799a81e7903a2663d9d79f41e5275de5b
Pedro Pureza <pedro.pureza@poatek.com>
2023-06-14 15:25:09 -0300

    faq page

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,9 @@
-import Link from "../src/components/Link";
-import Head from "next/head";
-import PageTitle from "../src/components/PageTitle";
+// faq.js
+import FAQScreen from "../src/screens/FAQScreen";
 
-export async function getStaticProps(context) {
+export default FAQScreen;
+
+export async function getStaticProps() {
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL)
@@ -12,26 +13,10 @@ export async function getStaticProps(context) {
     .then((response) => {
       return response;
     });
-  return {
-    props: { faq },
-  };
-}
 
-export default function FAQPage({ faq }) {
-  return (
-    <div>
-      <PageTitle>FAQ - Alura Cases</PageTitle>
-      <h1>Alura Cases - FAQ</h1>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return {
+    props: {
+      faq,
+    },
+  };
 }
