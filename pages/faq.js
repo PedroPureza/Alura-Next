a74a5c87b30c189e1d37402e5bca132d401b3d41
Pedro Pureza <pedro.pureza@poatek.com>
2023-06-14 13:43:39 -0300

    fix hydration problem and first faq page content implementation

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
 import Link from "../src/components/Link";
 
-export default function FAQPage() {
+export async function getStaticProps(context) {
+  return {
+    props: {},
+  };
+}
+
+export default function FAQPage(props) {
+  const [faq, setFaq] = useState([]);
+  useEffect(() => {
+    const FAQ_API_URL =
+      "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+    fetch(FAQ_API_URL)
+      .then((serverResponse) => {
+        return serverResponse.json();
+      })
+      .then((response) => {
+        setFaq(response);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Alura Cases - FAQ</h1>
-      <Link href="/">Go to Home page</Link>
+      <ul>
+        qw
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
+            <article>
+              <h2>{question}</h2>
+              <p>{answer}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/StyledLink/index.js
+++ src/components/StyledLink/index.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const StyledLink = React.forwardRef(({ onClick, href, children }, ref) => {
   return (
-    <>
-      <a href={href} onClick={onClick} ref={ref} style={{}}>
-        {children}
-      </a>
-    </>
+    <a href={href} onClick={onClick} ref={ref} style={{}}>
+      {children}
+    </a>
   );
 });
 
